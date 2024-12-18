5c585c60ffba22cd4be028960cecf656e6f6141c
Abhikumarimhere <abhishekkumar33822@gmail.com>
2024-12-18 19:04:53 +0530

    kafka

---
+++ src/main/java/com/Abhishek/CabbookUser/service/LocationService.java
@@ -0,0 +1,12 @@
+package com.Abhishek.CabbookUser.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+    @KafkaListener(topics = "cab-location", groupId = "user-group")
+    public void cablocation(String location){
+        System.out.println(location);
+    }
+}
---
+++ src/test/java/com/Abhishek/CabbookUser/CabBookUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Abhishek.CabbookUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
